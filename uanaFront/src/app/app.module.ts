41fe6d691e328d5c852d0f7e1faa3e6217ecaf75
GuilhermeRamosTeixeira <guilherme.98ramos@hotmail.com>
2022-02-21 11:50:29 -0300

    implementação do componente cadastrar

--- uanaFront/src/app/app.module.ts
+++ uanaFront/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { LoginComponent } from './login/login.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     RodapeComponent,
-    LoginComponent
+    LoginComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule,
