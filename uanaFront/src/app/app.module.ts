9ba432840ca9ae2a401d3a91829c984db2f32656
GuilhermeRamosTeixeira <guilherme.98ramos@hotmail.com>
2022-02-21 11:46:12 -0300

    implementação do componente login

--- uanaFront/src/app/app.module.ts
+++ uanaFront/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
