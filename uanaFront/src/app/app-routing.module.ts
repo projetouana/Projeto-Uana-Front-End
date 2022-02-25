c3481e53799ad2cd3eefd394e563c9c88c4ff198
larissa-oliv <lah.rio123@gmail.com>
2022-02-25 11:59:16 -0300

    Finalização do consumo da API

--- uanaFront/src/app/app-routing.module.ts
+++ uanaFront/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch:'full'},
+  {path: '', redirectTo: 'Home', pathMatch:'full'},
 
+  {path:'Home', component: HomeComponent},
   {path:'login', component: LoginComponent},
   {path:'cadastrar', component: CadastrarComponent},
   {path:'sobre' , component:SobreNosComponent}
