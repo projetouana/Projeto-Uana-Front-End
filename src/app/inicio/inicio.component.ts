e5017fdd54be0fb74314f41ff361a55fb3c3caa0
WilliRamon <acdc.ramon@hotmail.com>
2022-03-21 17:13:55 -0300

    Order By

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -25,6 +25,9 @@ export class InicioComponent implements OnInit {
   user: Usuario = new Usuario()
   idUser = environment.id
 
+  key = 'data'
+  reverse = true
+
   constructor(
     private router: Router,
     private postagemService: PostagemService,
